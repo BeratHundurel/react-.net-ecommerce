a994d7250d1bc421eea6259b4eaccad6c0e37a5e
Berat Hündürel <berat.hundurel@bilgiedu.net>
2023-10-15 22:55:37 +0300

    basket summary

--- client/src/features/basket/BasketPage.tsx
+++ client/src/features/basket/BasketPage.tsx
@@ -4,62 +4,69 @@ import { useStoreContext } from "../../app/context/StoreContext";
 import agent from "../../app/api/agent";
 import { useState } from "react";
 import { LoadingButton } from "@mui/lab";
+import BasketSummary from "./BasketSummary";
 
-export default function Home() {
+export default function BasketPage() {
     const { basket, setBasket, removeItem } = useStoreContext();
-    const [loading, setLoading] = useState(false);
-    function handleAddItem(productId: number) {
-        setLoading(true);
+    const [status, setStatus] = useState({
+        loading: false,
+        name: ''
+    });
+    function handleAddItem(productId: number, name: string) {
+        setStatus({ loading: true, name });
         agent.Basket.addItem(productId)
             .then(basket => setBasket(basket.value))
             .catch(error => console.log(error))
-            .finally(() => setLoading(false));
+            .finally(() => setStatus({ loading: false, name: '' }));
     }
 
-    function handleRemoveItem(productId: number, quantity = 1) {
-        setLoading(true);
+    function handleRemoveItem(productId: number, quantity = 1, name: string) {
+        setStatus({ loading: true, name });
         agent.Basket.removeItem(productId, quantity)
-            .then(() => removeItem(productId,quantity))
+            .then(() => removeItem(productId, quantity))
             .catch(error => console.log(error))
-            .finally(() => setLoading(false));
+            .finally(() => setStatus({ loading: false, name: '' }));
     }
 
     if (!basket) return <Typography variant="h3">Your basket is empty</Typography>
 
     return (
-        <div className="page-container">
-            {
-                basket.items.map(item => (
-                    <div className="basket-container" key={item.productId}>
-                        <div className="basket-img-container">
-                            <img src={item.pictureUrl} alt="product image" />
-                        </div>
-                        <div className="basket-description">
-                            <p>{item.name}</p>
-                        </div>
-                        <div className="counter">
-                            <Box sx={{ width: "100%", borderRadius: "10px", border: "2px solid #007247", padding: "10px", display: "flex", justifyContent: "space-between", alignItems: "center", gap: "10px", margin: "25px 0" }}>
-                                <LoadingButton onClick={() => handleRemoveItem(item.productId)} loading={loading} sx={{ minWidth: "25px", height: "25px" }} >
-                                    <Remove fontSize="small" sx={{ cursor: "pointer" }} />
-                                </LoadingButton>
+        <>
+            <div className="page-container">
+                {
+                    basket.items.map(item => (
+                        <div className="basket-container" key={item.productId}>
+                            <div className="basket-img-container">
+                                <img src={item.pictureUrl} alt="product image" />
+                            </div>
+                            <div className="basket-description">
+                                <p>{item.name}</p>
+                            </div>
+                            <div className="counter">
+                                <Box sx={{ width: "100%", borderRadius: "10px", border: "2px solid #007247", padding: "10px", display: "flex", justifyContent: "space-between", alignItems: "center", gap: "10px", margin: "25px 0" }}>
+                                    <LoadingButton onClick={() => handleRemoveItem(item.productId, 1, "rem" + item.productId)} loading={status.loading && status.name === "rem" + item.productId} sx={{ minWidth: "25px", height: "25px" }} >
+                                        <Remove fontSize="small" sx={{ cursor: "pointer", color: "rgb(69 69 69)" }} />
+                                    </LoadingButton>
 
-                                <Typography sx={{ color: "text.primary", fontWeight: "800", fontSize: "12px", borderRight: "2px solid #CDCDCD", borderLeft: "2px solid #CDCDCD", padding: "0 15px" }}>{item.quantity}</Typography>
-                                <LoadingButton onClick={() => handleAddItem(item.productId)} loading={loading} sx={{ minWidth: "25px", height: "25px" }}>
-                                    <Add fontSize="small" sx={{ cursor: "pointer" }} />
+                                    <Typography sx={{ color: "text.primary", fontWeight: "800", fontSize: "12px", borderRight: "2px solid #CDCDCD", borderLeft: "2px solid #CDCDCD", padding: "0 15px" }}>{item.quantity}</Typography>
+                                    <LoadingButton onClick={() => handleAddItem(item.productId, "add" + item.productId)} loading={status.loading && status.name === "add" + item.productId} sx={{ minWidth: "25px", height: "25px" }}>
+                                        <Add fontSize="small" sx={{ cursor: "pointer", color: "rgb(69 69 69)" }} />
+                                    </LoadingButton>
+                                </Box>
+                            </div>
+                            <div className="basket-price">
+                                <p>${((item.price / 100) * item.quantity).toFixed(2)}</p>
+                            </div>
+                            <div className="basket-remove">
+                                <LoadingButton onClick={() => handleRemoveItem(item.productId, item.quantity, "rem" + item.productId)} loading={status.loading && status.name === "rem" + item.productId} sx={{ minWidth: "25px", height: "25px" }} >
+                                    <Delete sx={{ color: "secondary.dark" }} />
                                 </LoadingButton>
-                            </Box>
+                            </div>
                         </div>
-                        <div className="basket-price">
-                            <p>{((item.price / 100) * item.quantity)}$</p>
-                        </div>
-                        <div className="basket-remove">
-                            <LoadingButton onClick={() => handleRemoveItem(item.productId, item.quantity)} loading={loading} sx={{ minWidth: "25px", height: "25px" }} >
-                                <Delete />
-                            </LoadingButton>
-                        </div>
-                    </div>
-                ))
-            }
-        </div>
+                    ))
+                }
+            </div>
+            <BasketSummary />
+        </>
     )
 }
---
+++ client/src/features/basket/BasketSummary.tsx
@@ -0,0 +1,31 @@
+import { useStoreContext } from "../../app/context/StoreContext"
+import { currencyFormat } from "../../app/util/util";
+
+export default function BasketSummary() {
+    const { basket } = useStoreContext();
+    const Total = basket?.items.reduce((sum, item) => sum + (item.price * item.quantity), 0) ?? 0;
+    const deliveryFee = Total > 10000 ? 0 : 500;
+    return (
+        <div className="basket-summary-container">
+            <div className="basket-coupon">
+                <div className="coupon-title">Use Coupon</div>
+                <div className="coupon-group">
+                    <input placeholder="Coupon Code" className="coupon-input" type="text" name="coupon-input" />
+                    <button className="coupon-btn">Apply</button>
+                </div>
+                <a href="#" className="basket-login">Don't have an account? Log in or sign up now</a>
+            </div>
+            <div className="basket-summary">
+                <div className="main-title">Basket Summary</div>
+                <div className="basket-box">
+                    <p>Delivery fee</p>
+                    <p>{currencyFormat(deliveryFee)}</p>
+                </div>
+                <div className="basket-box">
+                    <p>Total</p>
+                    <p>{currencyFormat(Total)}</p>
+                </div>
+            </div>
+        </div>
+    )
+}
